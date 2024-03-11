712504d08ba34aab10ac1a7a7bf5efbea0ec4314
vansh <vanshchinta03@gmail.com>
2024-03-11 15:31:17 +0530

    commit

--- app.js
+++ app.js
@@ -1,52 +1,22 @@
-import React, { Children } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
-import About from "./src/components/About";
-import Contact from "./src/components/Contact";
-import Error from "./src/components/Error";
-import RestaurantMenu from "./src/components/RestaurantMenu";
-
-
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-
 
   
+    
+
+
 const AppLayout = () =>{
     return(
     <div>
     <Header/>
-    <Outlet/>
+    <Body/>
     </div>
     )
 };
 
-const appRouter = createBrowserRouter([
-    {
-        path : "/",
-        element : <AppLayout/>,
-        errorElement : <Error/>,
-        children:[
-           {   
-                path: "/",
-               element: <Body/>,
-            },
-            {
-                path: "/about",
-                element: <About/>,
-            },
-            {
-                path: "/contact",
-                element: <Contact/>,
-            },
-            {
-                path: "/restaurants/:resId",
-                element: <RestaurantMenu/>,
-            },
-            ],
-      
-},
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router = {appRouter}/>);
+root.render(<AppLayout/>);
+
